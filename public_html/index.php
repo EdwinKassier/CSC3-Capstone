28a2e97438e257fad7d19b0f7f675fc7756a054c
Charl Ritter <CharlRitter@users.noreply.github.com>
2018-08-07 21:43:13 +0200

    Added login, navbar buttons  & background

--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,21 @@
-<?php require_once("../private/config.php"); ?>
+<?php require_once("../private/class.config.php"); ?>
 
 <html lang="en">
   <head>
     <?php include(TEMPLATE_FRONT . DS . "header.php"); ?>
+    <style>
+
+    .text-block {
+        text-align: center;
+        color: white;
+        background-color: rgba(0,0,0,0.7);
+        padding: 20px;
+    }
+
+    body {
+        background-image: url('../public_html/resources/images/homepage.png');        
+    }
+    </style>
   </head>
 
   <body>
@@ -13,10 +26,36 @@
 
       <!-- Main Body -->
       <br>
-      <main role="main" class="inner cover container" id="body">
-          <h1 class="cover-heading">Cover your page.</h1>
-          <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-          <p class="lead"><a href="#" class="btn btn-lg btn-secondary">Learn more</a></p>
+      <main class="row" id="body">
+        <div class="col-md-7" ></div>
+        <div class="col-md-3" style="margin: auto; padding: 120px 0;">
+          <div class="text-block" id="rcorners"> 
+            <!-- Login -->
+            <form class=""  method="post">
+              <div class="container">
+                  <h3>Login</h3>
+                  <?php //login_user(); ?>
+                  <small style="color:white;">Please fill in this form to log into your account.</small>
+                  <hr style="background-color:white;">
+                  <div class="form-group">
+                    <label for="login_email"><b>Email:</b></label>
+                    <input type="email" class="form-control" name="login_email" placeholder="Enter email" required>
+                  </div>
+                  <div class="form-group">
+                    <label for="login_password"><b>Password:</b></label>
+                    <input type="password" class="form-control" name="login_password" placeholder="Enter password" required>
+                  </div>
+                  <div class="form-check">
+                    <label class="form-check-label" style="padding-bottom:10px">
+                        <input class="form-check-input" type="checkbox" value="1" name="remember"> Remember me
+                    </label>
+                  </div>
+                  <button type="submit" name="login" class="col-md-6 btn btn-custom" >Login</button>
+                  <p class="text-muted" style="padding-top:10px"><a href="forgot.php?forgot=<?php //echo uniqid(true); ?>">Forgot password?</a></p>
+              </div>
+            </form>
+          </div>
+        </div>
       </main>
       <br>
 

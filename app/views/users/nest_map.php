fa1b1e9b75463ffbdb1376faee039174c9cef7ae
Edwin Kassier <edwinkassier@gmail.com>
2018-08-16 11:45:40 +0200

    Map fix
    Fixed the main nest_map page
    -added scouting location list
    -Adding scouting sites and add nest site buttons (no functionality, but with icons)

--- app/views/users/nest_map.php
+++ app/views/users/nest_map.php
@@ -11,19 +11,30 @@
 
     <!-- Main Body -->
     <br>
-    <main role="main" class="container" id="body">
-
-        <div class="row" style="max-width:100%; overflow-x: hidden;overflow-y: hidden;">
-
-            <div class="col-md-2">
-                <h3>All map functions go here</h3>
+    <main role="main" id="body">
+  <div class = "row">
+        <div class="col-md-3" style="padding-left:50px;">
+            <h3>My scouting locations</h3>
+            <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-action">Scouting location 1 - (lat,long)</a>
+                <a href="#" class="list-group-item list-group-item-action">Scouting location 2 - (lat,long) </a>
+                <a href="#" class="list-group-item list-group-item-action">Scouting location 3 - (lat,long)</a>
+                <a href="#" class="list-group-item list-group-item-action">Scouting location 4 - (lat,long)</a>
             </div>
-
-            <div class="col-md-10">
-                <div id="googleMap" style="width:100%;height:100vh;"></div>
-            </div>
-
+            <hr>
+            <h3>Add sites</h3>
+            <br>
+            <button type="button" class="btn btn-custom" style="width:100%;">Add scouting site  <i class="fas fa-flag" style="color:red;"></i></i></button>
+            <br>
+            <br>
+            <button type="button" class="btn btn-custom" style="width:100%;">Add nest site  <i class="fas fa-map-pin" style="color:red;"></i></i></i></button>
         </div>
+
+        <div class="col-md-9">
+            <div id="googleMap" class="z-depth-1" style="height:100%;"></div>
+        </div>
+  </div>
+
 
     </main>
     <br>
@@ -34,14 +45,24 @@
 </div>
 <!-- Javascript -->
 <script>
+
     function myMap() {
-        var mapProp= {
-            center:new google.maps.LatLng(-33.958732, 18.460068),
-            zoom:6,
-            mapTypeId: google.maps.MapTypeId.HYBRID
+        var var_location = new google.maps.LatLng(-33.958732, 18.460068);
+
+        var var_mapoptions = {
+            center: var_location,
+            zoom: 8,
+            mapTypeId: 'satellite'
         };
-        var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+        var map = new google.maps.Map(document.getElementById("googleMap"),var_mapoptions);
+
+
     }
+
+    // Initialize maps
+    google.maps.event.addDomListener(window, 'load', myMap);
+
 </script>
 </body>
 </html>
